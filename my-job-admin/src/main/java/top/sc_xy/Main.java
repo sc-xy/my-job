fe7ea135fbfca229a498fb196dfd66ae9b1167dc
sc-xy <3148537853@qq.com>
2024-07-22 00:16:56 +0800

    搭建框架

---
+++ my-job-admin/src/main/java/top/sc_xy/Main.java
@@ -0,0 +1,11 @@
+package top.sc_xy;
+
+/**
+ * @author sc-xy
+ * @time 2024/7/22
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
